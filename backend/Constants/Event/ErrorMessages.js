0d9d91260ca20618c5d6c256f83d95a2cb048f56
Firas-FH <firas2001hssan22@gmail.com>
2024-07-25 20:06:56 +0300

    edit backend

--- backend/Constants/Event/ErrorMessages.js
+++
@@ -1,9 +0,0 @@
-// ErrorMessages.js
-
-const ErrorMessages = {
-    UNAUTHORIZED_ERROR: 'Unauthorized access. Please log in.',
-    // Add more error messages as needed
-  };
-  
-  module.exports = ErrorMessages;
-  
